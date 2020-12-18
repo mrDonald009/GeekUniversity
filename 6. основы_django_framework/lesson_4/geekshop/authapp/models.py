3c3bacee372c008a6c608c5fadd94720b300a468
mrDon <mrdonald009@gmail.com>
2020-12-18 13:58:55 +0300

    delete_.idea

--- 6. основы_django_framework/lesson_4/geekshop/authapp/models.py
+++ 6. основы_django_framework/lesson_4/geekshop/authapp/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatar', blank=True)
-    age  = models.PositiveSmallIntegerField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
