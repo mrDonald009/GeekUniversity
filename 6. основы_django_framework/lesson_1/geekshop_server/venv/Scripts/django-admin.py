4ec4080f6010c183952bf76c9e73e1d1a7d9daad
mrDon <mrdonald009@gmail.com>
2021-02-18 15:27:15 +0300

    add_django_framework_lesson_1

---
+++ 6. основы_django_framework/lesson_1/geekshop_server/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\geekuniversity\6. основы_django_framework_repeat\lesson_1\geekshop_server\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
