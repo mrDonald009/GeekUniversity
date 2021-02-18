dbc1bbf6effe2aca1f20c5f0b799e2225ed037a5
mrDonald009 <34665724+mrDonald009@users.noreply.github.com>
2021-02-18 15:35:24 +0300

    Delete 6. основы_django_framework directory

--- 6. основы_django_framework/lesson_1/geekshop_server/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\admin\geekuniversity\6. основы_django_framework_repeat\lesson_1\geekshop_server\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
